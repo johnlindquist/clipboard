dc8ec68b36b4da641ac6f289834bea7795d62d2b
John Lindquist <johnlindquist@gmail.com>
2023-03-27 15:09:50 -0600

    fix(build): fix x64 in makefile, update deps

--- go.mod
+++ go.mod
@@ -2,12 +2,15 @@ module github.com/CrossCopy/clipboard
 
 go 1.19
 
-require golang.design/x/clipboard v0.6.2
+require (
+	github.com/ZacJoffe/clipboard v0.0.0-20190414232103-e1d59897bf29
+	golang.design/x/clipboard v0.7.0
+)
 
 require (
-	github.com/ZacJoffe/clipboard v0.0.0-20190414232103-e1d59897bf29 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
-	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/exp/shiny v0.0.0-20230321023759-10a507213a29 // indirect
+	golang.org/x/image v0.6.0 // indirect
+	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
--- index.ts
+++ index.ts
@@ -1,135 +1,117 @@
-import os from "node:os";
-import fs from "node:fs";
-import path from "node:path";
-import { EventEmitter } from "node:events";
-import net from "net";
-import { execFile, ChildProcess, execFileSync } from "node:child_process";
-import util from "util";
-import { base64BufToUTF8, base64StringToUTF8 } from "./src/util";
-import { ClipboardOperation, WatchEvent } from "./src/constants";
+import os from "node:os"
+import fs from "node:fs"
+import path from "node:path"
+import detectPort from "detect-port"
+import { EventEmitter } from "node:events"
+import net from "net"
+import { execFile, ChildProcess, execFileSync } from "node:child_process"
+import util from "util"
+import { base64BufToUTF8, base64StringToUTF8 } from "./src/util"
+import { ClipboardOperation, WatchEvent } from "./src/constants"
 
-const execFileAsync = util.promisify(execFile);
+const execFileAsync = util.promisify(execFile)
 
 export class Clipboard {
-  child: ChildProcess | undefined;
-  server: net.Server | undefined;
-  emitter: EventEmitter;
+  child: ChildProcess | undefined
+  server: net.Server | undefined
+  emitter: EventEmitter
 
   constructor() {
-    this.child = undefined;
-    this.emitter = new EventEmitter();
+    this.child = undefined
+    this.emitter = new EventEmitter()
   }
 
   get exeFilename(): string {
-    const { platform, arch } = process;
-    let exeFilename = `go-clipboard-${platform}-${arch}`;
+    const { platform, arch } = process
+    let exeFilename = `go-clipboard-${platform}-${arch}`
     if (platform === "win32") {
-      exeFilename += ".exe";
+      exeFilename += ".exe"
     }
-    return exeFilename;
+    return exeFilename
   }
 
   get goClipboardPath(): string {
-    const pathArr = [__dirname];
+    const pathArr = [__dirname]
 
     if (path.basename(__dirname) === "dist") {
-      pathArr.push("..");
+      pathArr.push("..")
     }
 
-    pathArr.push(...["go-clipboard", "binaries", this.exeFilename]);
-    const exePath = path.join(...pathArr);
-    return exePath;
+    pathArr.push(...["go-clipboard", "binaries", this.exeFilename])
+    const exePath = path.join(...pathArr)
+    return exePath
   }
 
   readText(): Promise<string> {
-    return execFileAsync(this.goClipboardPath, [
-      ClipboardOperation.READ_TEXT,
-    ]).then((result) => {
-      return base64StringToUTF8(result.stdout);
-    });
+    return execFileAsync(this.goClipboardPath, [ClipboardOperation.READ_TEXT]).then(result => {
+      return base64StringToUTF8(result.stdout)
+    })
   }
 
   readTextSync(): string {
-    const buf = execFileSync(this.goClipboardPath, [
-      ClipboardOperation.READ_TEXT,
-    ]);
-    return base64BufToUTF8(buf);
+    const buf = execFileSync(this.goClipboardPath, [ClipboardOperation.READ_TEXT])
+    return base64BufToUTF8(buf)
   }
 
   writeText(content: string): Promise<void> {
-    return Promise.resolve(this.writeTextSync(content));
+    return Promise.resolve(this.writeTextSync(content))
   }
 
   writeTextSync(content: string): void {
-    const writeTextProcess = execFileSync(
-      this.goClipboardPath,
-      [ClipboardOperation.WRITE_TEXT],
-      {
-        input: Buffer.from(content).toString('base64'),
-        // stdio: "inherit",
-        // TODO: consider using stdio to get stderr using pipe and handle error
-        // stderr should be auto piped to parent process
-        // https://nodejs.org/api/child_process.html#child_processexecfilesyncfile-args-options
-        // stdio: ['pipe']
-      }
-    );
+    const writeTextProcess = execFileSync(this.goClipboardPath, [ClipboardOperation.WRITE_TEXT], {
+      input: Buffer.from(content).toString("base64"),
+      // stdio: "inherit",
+      // TODO: consider using stdio to get stderr using pipe and handle error
+      // stderr should be auto piped to parent process
+      // https://nodejs.org/api/child_process.html#child_processexecfilesyncfile-args-options
+      // stdio: ['pipe']
+    })
   }
 
-  writeImageSync(base64ImgStr: string): void;
-  writeImageSync(imgBuf: Buffer): void;
+  writeImageSync(base64ImgStr: string): void
+  writeImageSync(imgBuf: Buffer): void
   writeImageSync(data: string | Buffer): void {
     if (data instanceof String || typeof data === "string") {
       if (process.platform === "win32") {
-        const tmpDir = os.tmpdir();
-        const imgPath = path.join(tmpDir, "tmp.png");
-        const imgBuf = Buffer.from(data, "base64");
-        fs.writeFileSync(imgPath, imgBuf);
-        const scriptPath = path.join(
-          this.goClipboardPath,
-          "..",
-          "..",
-          "scripts",
-          "win",
-          "write_image.ps1"
-        );
-        console.log(scriptPath);
-        execFileSync("powershell", [scriptPath, "-imagePath", imgPath]);
+        const tmpDir = os.tmpdir()
+        const imgPath = path.join(tmpDir, "tmp.png")
+        const imgBuf = Buffer.from(data, "base64")
+        fs.writeFileSync(imgPath, imgBuf)
+        const scriptPath = path.join(this.goClipboardPath, "..", "..", "scripts", "win", "write_image.ps1")
+        console.log(scriptPath)
+        execFileSync("powershell", [scriptPath, "-imagePath", imgPath])
       } else {
-        const writeImageProcess = execFileSync(
-          this.goClipboardPath,
-          [ClipboardOperation.WRITE_IMAGE],
-          {
-            input: data,
-            // TODO: consider using stdio to get stderr using pipe and handle error
-            // https://nodejs.org/api/child_process.html#child_processexecfilesyncfile-args-options
-            // stdio:
-          }
-        );
+        const writeImageProcess = execFileSync(this.goClipboardPath, [ClipboardOperation.WRITE_IMAGE], {
+          input: data,
+          // TODO: consider using stdio to get stderr using pipe and handle error
+          // https://nodejs.org/api/child_process.html#child_processexecfilesyncfile-args-options
+          // stdio:
+        })
       }
     } else if (data instanceof Buffer) {
-      this.writeImageSync(data.toString("base64"));
+      this.writeImageSync(data.toString("base64"))
     } else {
-      throw new Error("Invalid Data Type. Has to be string or Buffer");
+      throw new Error("Invalid Data Type. Has to be string or Buffer")
     }
   }
-  writeImage(data: string): Promise<void>;
-  writeImage(data: Buffer): Promise<void>;
+  writeImage(data: string): Promise<void>
+  writeImage(data: Buffer): Promise<void>
   writeImage(data: string | Buffer): Promise<void> {
     if (data instanceof String || typeof data === "string") {
-      return Promise.resolve(this.writeImageSync(data as string));
+      return Promise.resolve(this.writeImageSync(data as string))
     } else if (data instanceof Buffer) {
-      return Promise.resolve(this.writeImageSync(data as Buffer));
+      return Promise.resolve(this.writeImageSync(data as Buffer))
     } else {
-      throw new Error("Invalid Data Type. Has to be string or Buffer");
+      throw new Error("Invalid Data Type. Has to be string or Buffer")
     }
   }
 
   readImage(): Promise<Buffer> {
-    return Promise.resolve(this.readImageSync());
+    return Promise.resolve(this.readImageSync())
   }
 
   readImageBase64(): Promise<string> {
-    return Promise.resolve(this.readImageBase64Sync());
+    return Promise.resolve(this.readImageBase64Sync())
   }
 
   readImageSync(): Buffer {
@@ -144,23 +126,21 @@ export class Clipboard {
     //   const buf = execFileSync('powershell', [scriptPath])
     //   return Buffer.from(buf.toString(), 'base64')
     // }
-    const buf = execFileSync(this.goClipboardPath, [
-      ClipboardOperation.READ_IMAGE,
-    ]);
-    const stdoutStr = buf.toString();
-    const imgBuf = Buffer.from(stdoutStr, "base64");
+    const buf = execFileSync(this.goClipboardPath, [ClipboardOperation.READ_IMAGE])
+    const stdoutStr = buf.toString()
+    const imgBuf = Buffer.from(stdoutStr, "base64")
     // fs.writeFileSync("test.png", imgBuf); // for debugging only
-    return imgBuf;
+    return imgBuf
   }
 
   readImageBase64Sync(): string {
-    return this.readImageSync().toString("base64");
+    return this.readImageSync().toString("base64")
   }
 
   listen() {
-    this.server = net.createServer((con) => {
+    this.server = net.createServer(con => {
       // client connected
-      let data = "";
+      let data = ""
 
       // * demo of sending message to client,
       // con.write("close");
@@ -168,61 +148,60 @@ export class Clipboard {
       /**
        * Accumulate data by concatenating chunks of received data
        */
-      con.on("data", (packet) => {
-        data += packet.toString();
-      });
+      con.on("data", packet => {
+        data += packet.toString()
+      })
 
       /**
        * When socket connection closes, wrap up received data
        */
       con.on("close", () => {
-        const strData = data.toString();
-        const subStr = strData.substring(0, 14);
+        const strData = data.toString()
+        console.log({ strData })
+        const subStr = strData.substring(0, 14)
         if (subStr.includes(`${WatchEvent.TEXT_CHANGED}:`)) {
-          const text = Buffer.from(strData.substring(13), "base64").toString();
-          this.emitter.emit("text", text);
+          const text = Buffer.from(strData.substring(13), "base64").toString()
+          this.emitter.emit("text", text)
         } else if (subStr.includes(`${WatchEvent.IMAGE_CHANGED}:`)) {
-          const data = strData.substring(14) as string;
-          this.emitter.emit("image", Buffer.from(data, "base64"));
+          const data = strData.substring(14) as string
+          this.emitter.emit("image", Buffer.from(data, "base64"))
           // const imageBase64 = data.toString("base64");
           // console.log(`Image Changed`);
           // fs.writeFileSync("test.png", Buffer.from(data, "base64"));
         }
-      });
-    });
+      })
+    })
 
-    this.server.listen(19559, () => {
-      const addr = this.server?.address();
-      if (!addr)
-        throw new Error("Unexpected Error: TCP Socket Server not Started");
-      const port = (addr as net.AddressInfo).port;
-      this.child = execFile(this.goClipboardPath, [port.toString()]);
-      // this.child.stdout?.on("data", (data) => {
-      //   console.log("Received data from client socket stdout:\n" + data);
-      // });
-    });
+    detectPort(19559, (err, port) => {
+      this?.server?.listen(port, () => {
+        const addr = this.server?.address()
+        if (!addr) throw new Error("Unexpected Error: TCP Socket Server not Started")
+        const port = (addr as net.AddressInfo).port
+        this.child = execFile(this.goClipboardPath, [port.toString()])
+        // this.child.stdout?.on("data", (data) => {
+        //   console.log("Received data from client socket stdout:\n" + data);
+        // });
+      })
+    })
   }
 
   close() {
-    this.emitter.emit("close");
-    this.emitter.removeAllListeners();
-    this.server?.close();
-    const res = this.child?.kill();
-    return res;
+    this.emitter.emit("close")
+    this.emitter.removeAllListeners()
+    this.server?.close()
+    const res = this.child?.kill()
+    return res
   }
 
-  on(event: "text", cb: TextCallback): void;
-  on(event: "image", cb: ImageCallback): void;
-  on(event: "close", cb: () => void): void;
-  on(
-    event: "text" | "image" | "close",
-    cb: ImageCallback | TextCallback
-  ): void {
-    this.emitter.on(event, cb);
+  on(event: "text", cb: TextCallback): void
+  on(event: "image", cb: ImageCallback): void
+  on(event: "close", cb: () => void): void
+  on(event: "text" | "image" | "close", cb: ImageCallback | TextCallback): void {
+    this.emitter.on(event, cb)
   }
 }
 
-type TextCallback = (data: string) => void;
-type ImageCallback = (data: Buffer) => void;
+type TextCallback = (data: string) => void
+type ImageCallback = (data: Buffer) => void
 
-export default new Clipboard();
+export default new Clipboard()
